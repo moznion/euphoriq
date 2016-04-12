8746454e965ba1f76671460b7c6570d2727ca929
moznion <moznion@gmail.com>
2016-04-12 17:38:02 +0900

    Support ObjectMapper by Job class

--- src/main/java/net/moznion/euphoriq/Job.java
+++ src/main/java/net/moznion/euphoriq/Job.java
@@ -1,12 +1,14 @@
 package net.moznion.euphoriq;
 
+import java.util.OptionalInt;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.OptionalInt;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Job {
     private long id;
     private Object arg;
